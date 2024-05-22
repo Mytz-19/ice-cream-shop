486646b4bfbaf83dd9aab42507eb5d66e7add0ae
APAC\YHM1KOR <Maytri.Sharma@in.bosch.com>
2024-05-22 16:08:01 +0530

    Allow origins in CORS

--- backend/fastapi_app.py
+++ backend/fastapi_app.py
@@ -1,9 +1,11 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.routers.employees import router as employee_router
 from app.routers.products import router as product_router
+from app.routers.orders import router as order_router
 from db.db_manager import DBManager
 
 
@@ -16,7 +18,16 @@ async def lifespan(app: FastAPI):
     # Release the resources
     db_manager.close()
 
+
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Include the employees router
 app.include_router(employee_router, tags=["Employee"])
